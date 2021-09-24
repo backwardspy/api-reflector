710682c8ab1461061e7da4a8d07ec932ad136e6e
Chris Latham <backwardspy@gmail.com>
2021-09-24 15:37:20 +0100

    automatic db schema migration on startup

--- api_reflector/db.py
+++ api_reflector/db.py
@@ -3,7 +3,14 @@ Creates the flask-sqlalchemy instance and aliases its Model and session properti
 """
 
 from flask_sqlalchemy import SQLAlchemy
+from alembic.config import Config
+from alembic import command
 
 sqla = SQLAlchemy()
 Model = sqla.Model
 session = sqla.session
+
+
+def run_migrations() -> None:
+    cfg = Config("alembic.ini")
+    command.upgrade(cfg, "head")
